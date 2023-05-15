c3821f59eb492747f068a6de37a7b749469723ed
Oleksii Chubenko <i1723@i.ua>
2023-05-15 22:38:52 +0300

    fixed

--- src/redux/contacts/contacts.thunk.js
+++ src/redux/contacts/contacts.thunk.js
@@ -1,35 +1,44 @@
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const contactsApi = axios.create({
-    baseURL: "https://63e4ff1ac04baebbcdb10925.mockapi.io",
-})
+  baseURL: 'https://646280ed7a9eead6fad0f8fe.mockapi.io',
+});
 
-export const fetchContactsThunk = createAsyncThunk("contacts/fetchAll", async (_, thunkAPI) => {
+export const fetchContactsThunk = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
     try {
-        const { data } = await contactsApi.get("/contacts");
-        return data;
+      const { data } = await contactsApi.get('/contacts');
+      return data;
     } catch (error) {
-        console.log(error);
-        return thunkAPI.rejectWithValue(error)
+      console.log(error);
+      return thunkAPI.rejectWithValue(error);
     }
-})
+  }
+);
 
-export const addContactThunk = createAsyncThunk("contacts/addContact", async (contact, thunkAPI) => {
+export const addContactThunk = createAsyncThunk(
+  'contacts/addContact',
+  async (contact, thunkAPI) => {
     try {
-        const { data } = await contactsApi.post("/contacts", contact);
-        return data
+      const { data } = await contactsApi.post('/contacts', contact);
+      return data;
     } catch (error) {
-        console.log(error);
-        return thunkAPI.rejectWithValue(error)
+      console.log(error);
+      return thunkAPI.rejectWithValue(error);
     }
-})
-export const deleteContactThunk = createAsyncThunk("contacts/deleteContact", async (id, thunkAPI) => {
+  }
+);
+export const deleteContactThunk = createAsyncThunk(
+  'contacts/deleteContact',
+  async (id, thunkAPI) => {
     try {
-        const { data } = await contactsApi.delete(`/contacts/${id}`);
-        return data.id;
+      const { data } = await contactsApi.delete(`/contacts/${id}`);
+      return data.id;
     } catch (error) {
-        console.log(error);
-        return thunkAPI.rejectWithValue(error)
+      console.log(error);
+      return thunkAPI.rejectWithValue(error);
     }
-})
+  }
+);
